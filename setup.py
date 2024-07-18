1b87665001d93c64e944f8ada7be32ef20e639c7
swapniljyt <swapniljytkd888@gmail.com>
2024-07-18 17:26:14 +0000

    commit all changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='NLP-BASED-ARTICLE-ANALYZER',
+    version='0.0.1',
+    author='Swapnil Jyot',
+    author_email='swapniljytkd888@gmail.com',
+    install_requires=[],
+    packages=find_packages()
+)
